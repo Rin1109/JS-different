d8aa054fb0a8361708e9b804a40f441da321acb6
Iryna <gresrin@gmail.com>
2022-08-05 14:09:03 +0200

    Work with swiper (v3)

--- popup.js
+++ popup.js
@@ -12,7 +12,25 @@ var swiper = new Swiper(".card_slider", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
       },
-   });
+});
+   
+var swiper = new Swiper(".mySwiper", {
+   initialSlide: 4,
+   effect: "coverflow",
+   grabCursor: true,
+   centeredSlides: true,
+   slidesPerView: "auto",
+   coverflowEffect: {
+   rotate: 50,
+   stretch: 0,
+   depth: 100,
+   modifier: 1,
+   slideShadows: true,
+},
+pagination: {
+   el: ".swiper-pagination",
+},
+});
 
 
 const popupLinks = document.querySelectorAll('.popup-link');
